40a3408f9d06af1563bed4e360e28e866826d778
user <sanahaju123@gmail.com>
2023-11-27 13:56:20 +0530

    Updated

---
+++ ScoreMangementApp/Migrations/202311270823293_initial.cs
@@ -0,0 +1,30 @@
+﻿namespace ScoreManagementApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Scores",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Date = c.DateTime(nullable: false),
+                        PlayerName = c.String(),
+                        PlayerScore = c.Int(nullable: false),
+                        GameType = c.String(),
+                        Notes = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Scores");
+        }
+    }
+}
